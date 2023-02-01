78e8a10a53d44d282d8b1e6e64fd0cf4b9ba3821
aniketjha9179 <aniketjha9179@gmail.com>
2023-02-01 17:27:26 +0530

    first commit

---
+++ boolean.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    bool exp1=true;
+    bool exp2= false;
+    cout<<(exp1&&exp2)<<endl;//false
+    cout<<(exp1||exp2)<<endl;
+    cout<<(!exp1)<<endl;
+    int num1=6;
+    int num2=3;
+    num1+=3;
+    cout<<num1<<endl;
+    num2-=2;
+    cout<<num2<<endl;
+    
+
+ }
---
+++ helllo.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+    puts("Click on Button");
+    puts("Create a new player");
+    puts("add life to player");
+    return 0;//return an exit with 0 code 
+}
+
+// this is for refrence 
+void nothing(){
+    puts("hello world");
+}
+/*
+this is multiline 
+comment
+*/
+
+char  iamachar(){
+    return 'h';
+}
